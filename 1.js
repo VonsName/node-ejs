88f9499b45c5fa41ac487f5c369570fe947431b1
fjl <fengjianglin6817@dingtalk.com>
2018-06-08 16:24:25 +0800

    Initial commit

---
+++ 1.js
@@ -0,0 +1,14 @@
+const ejss=require('ejs');
+const fs=require('fs');
+const str=ejss.renderFile('./views/1.ejs',
+    {json:{arr:[{'name':'李四','age':23},
+                {'name':'王五','age':28},
+                {'name':'张三','age':25}]}},
+                function (err,data) {
+    console.log(data);
+});
+fs.writeFile('./views/1.html',str,function (err) {
+    if (err){
+        console.log('编译失败');
+    }
+});
